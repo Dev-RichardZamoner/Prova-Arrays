7756c6e1e64ec110cc0a260ce1710641ca234ac3
Richard T S Zamoner <130820445+Dev-RichardZamoner@users.noreply.github.com>
2023-09-29 00:54:48 -0300

    Update ArrayTeste.java

--- ArrayTeste.java
+++ ArrayTeste.java
@@ -1,64 +1,49 @@
 import java.util.Scanner;
 
-class Array {
-    private int[] array;
-
-    public Array(int tamanho) {
-        array = new int[tamanho];
-    }
-
-    public void adicionarElemento(int posicao, int valor) {
-        if (posicao >= 0 && posicao < array.length) {
-            array[posicao] = valor;
-        } else {
-            System.out.println("Posição inválida.");
-        }
-    }
-
-    public void listarElementos() {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Elemento na posição " + i + ": " + array[i]);
-        }
-    }
-}
-
 public class ArrayTeste {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Informe o tamanho do array: ");
-        int tamanho = scanner.nextInt();
-
-        Array meuArray = new Array(tamanho);
+        System.out.print("Digite o tamanho do array: ");
+        int capacidade = scanner.nextInt();
+        Array vetor = new Array(capacidade);
 
         int opcao;
         do {
             System.out.println("\nMenu:");
             System.out.println("1. Adicionar Elemento");
-            System.out.println("2. Listar Elementos");
-            System.out.println("3. Sair");
+            System.out.println("2. Remover Elemento");
+            System.out.println("3. Alterar Elemento");
+            System.out.println("4. Listar Elementos");
+            System.out.println("5. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.print("Informe a posição: ");
-                    int posicao = scanner.nextInt();
-                    System.out.print("Informe o valor: ");
-                    int valor = scanner.nextInt();
-                    meuArray.adicionarElemento(posicao, valor);
+                    System.out.print("Digite o elemento a ser adicionado: ");
+                    int elementoAdicionar = scanner.nextInt();
+                    vetor.adicionarElemento(elementoAdicionar);
                     break;
                 case 2:
-                    meuArray.listarElementos();
+                    vetor.removerElemento();
                     break;
                 case 3:
-                    System.out.println("Encerrando o programa.");
+                    System.out.print("Digite a posição do elemento a ser alterado: ");
+                    int posicao = scanner.nextInt();
+                    System.out.print("Digite o novo elemento: ");
+                    int novoElemento = scanner.nextInt();
+                    vetor.alterarElemento(posicao, novoElemento);
+                    break;
+                case 4:
+                    vetor.listarElementos();
+                    break;
+                case 5:
+                    System.out.println("Encerrando a aplicação.");
                     break;
                 default:
-                    System.out.println("Opção inválida.");
+                    System.out.println("Opção inválida. Tente novamente.");
             }
-
-        } while (opcao != 3);
+        } while (opcao != 5);
 
         scanner.close();
     }
